a6f2ea0d70631204ff6e466fd8e30710087d7eb9
Tom Short <tshort.rlists@gmail.com>
2012-05-20 07:55:45 -0400

    Work on Winston plotting and add a breaking pendulum in a box.

---
+++ examples/breaking_pendulum_in_box.jl
@@ -0,0 +1,57 @@
+
+load("../src/sims.jl")
+
+############################################
+## Breaking pendulum with floor and walls ##
+############################################
+
+function FreeFall(x,y,vx,vy)
+    {
+     vx - der(x)
+     vy - der(y)
+     der(vx)
+     der(vy) + 9.81
+     Event(y + 1.01,                # Bounce up if it hits the floor
+           {reinit(vy, 0.95 * abs(vy))},
+           {reinit(vy, 0.95 * abs(vy))})
+     Event(x + 1.01,                # Left wall
+           {reinit(vx, 0.95 * abs(vx))},
+           {reinit(vx, 0.95 * abs(vx))})
+     Event(x - 1.01,                # Right wall
+           {reinit(vx, -0.95 * abs(vx))},
+           {reinit(vx, -0.95 * abs(vx))})
+    }
+end
+
+function Pendulum(x,y,vx,vy)
+    len = sqrt(x.value^2 + y.value^2)
+    phi0 = atan2(x.value, -y.value) 
+    phi = Unknown(phi0)
+    phid = Unknown()
+    {
+     phid - der(phi)
+     vx - der(x)
+     vy - der(y)
+     x - len * sin(phi)
+     y + len * cos(phi)
+     der(phid) + 9.81 / len * sin(phi)
+    }
+end
+
+function BreakingPendulum()
+    x = Unknown(cos(pi/4), "x")
+    y = Unknown(-cos(pi/4), "y")
+    vx = Unknown()
+    vy = Unknown()
+    {
+     StructuralEvent(MTime - 1.8,     # when time hits 1.8 sec, switch to FreeFall
+         {:(FreeFall($x,$y,$vx,$vy))},
+         Pendulum(x,y,vx,vy))
+    }
+end
+
+p = BreakingPendulum()
+p_f = elaborate(p)
+p_s = create_sim(p_f) 
+p_y = sim(p_s, 5.0)  
+
