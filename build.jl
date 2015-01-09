e655a0a9b1803b86108ffa6739e6bebd1287002a
Tom Short <tshort.rlists@gmail.com>
2015-01-08 19:40:33 -0500

    Update documentation

---
+++ build.jl
@@ -0,0 +1,6 @@
+using Lexicon, Sims
+save("api.md", Sims)
+save("lib.md", Sims.Lib)
+save("examples/basics.md", Sims.Examples.Basics)
+save("examples/lib.md", Sims.Examples.Lib)
+save("examples/neural.md", Sims.Examples.Neural)
