c390e3312ee243f68471da0006b64ddaeb1ee87c
Tom Short <tshort.rlists@gmail.com>
2016-12-06 14:41:10 -0500

    Revert Reactive to test effect on Event bugs

--- src/utils.jl
+++ src/utils.jl
@@ -39,21 +39,21 @@ Plots.@recipe function f(x::SimResult; columns = collect(1:length(x.colnames)))
     x.y[:, 1], x.y[:, 1+columns]
 end
 
-z = sim(Sims.Examples.Lib.CauerLowPassOPV2(), 60.0)
+# z = sim(Sims.Examples.Lib.CauerLowPassOPV2(), 60.0)
 
-plot(z)
-plot(z, collect(1:length(z.colnames)))
-plot(z, r".*", title = "CauerLowPassOPV")
-plot(z, subplots = true, title = "subplots = true")
-plot(z, 9:11, subplots = false, title = "subplots = false")
-plot(z, r"n", title = "r\"n\"")
-plot(z, r"n1", title = "r\"n1\"")
-plot(z, 5:8, legend = false)
-figure()
-plot(z, 5:8, legend = false, newfigure = false)
-plot(z, ["n8", "n9"], title = string(["n8", "n9"]))
-plot(z, [("n8", "n9"), ("n10", "n11")], title = string([("n8", "n9"), ("n10", "n11")]))
-plot(z, [r"n1", ("n9", "n10")], title = string([r"n1", ("n9", "n10")]))
+# plot(z)
+# plot(z, collect(1:length(z.colnames)))
+# plot(z, r".*", title = "CauerLowPassOPV")
+# plot(z, subplots = true, title = "subplots = true")
+# plot(z, 9:11, subplots = false, title = "subplots = false")
+# plot(z, r"n", title = "r\"n\"")
+# plot(z, r"n1", title = "r\"n1\"")
+# plot(z, 5:8, legend = false)
+# figure()
+# plot(z, 5:8, legend = false, newfigure = false)
+# plot(z, ["n8", "n9"], title = string(["n8", "n9"]))
+# plot(z, [("n8", "n9"), ("n10", "n11")], title = string([("n8", "n9"), ("n10", "n11")]))
+# plot(z, [r"n1", ("n9", "n10")], title = string([r"n1", ("n9", "n10")]))
 # """
 # Plot simulation result with PyPlot (must be installed and
 # loaded).
@@ -115,21 +115,21 @@ plot(z, [r"n1", ("n9", "n10")], title = string([r"n1", ("n9", "n10")]))
 # 
 # ```julia
 # using Sims
-z = sim(Sims.Examples.Lib.CauerLowPassOPV2(), 60.0)
+# z = sim(Sims.Examples.Lib.CauerLowPassOPV2(), 60.0)
 
-plot(z)
-plot(z, collect(1:length(z.colnames)))
-plot(z, r".*", title = "CauerLowPassOPV")
-plot(z, subplots = true, title = "subplots = true")
-plot(z, 9:11, subplots = false, title = "subplots = false")
-plot(z, r"n", title = "r\"n\"")
-plot(z, r"n1", title = "r\"n1\"")
-plot(z, 5:8, legend = false)
-figure()
-plot(z, 5:8, legend = false, newfigure = false)
-plot(z, ["n8", "n9"], title = string(["n8", "n9"]))
-plot(z, [("n8", "n9"), ("n10", "n11")], title = string([("n8", "n9"), ("n10", "n11")]))
-plot(z, [r"n1", ("n9", "n10")], title = string([r"n1", ("n9", "n10")]))
+# plot(z)
+# plot(z, collect(1:length(z.colnames)))
+# plot(z, r".*", title = "CauerLowPassOPV")
+# plot(z, subplots = true, title = "subplots = true")
+# plot(z, 9:11, subplots = false, title = "subplots = false")
+# plot(z, r"n", title = "r\"n\"")
+# plot(z, r"n1", title = "r\"n1\"")
+# plot(z, 5:8, legend = false)
+# figure()
+# plot(z, 5:8, legend = false, newfigure = false)
+# plot(z, ["n8", "n9"], title = string(["n8", "n9"]))
+# plot(z, [("n8", "n9"), ("n10", "n11")], title = string([("n8", "n9"), ("n10", "n11")]))
+# plot(z, [r"n1", ("n9", "n10")], title = string([r"n1", ("n9", "n10")]))
 # 
 # ```
 # 
